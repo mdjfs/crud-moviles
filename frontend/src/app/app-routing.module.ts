26513d346e6cab07a684f74f04817f8ee8110d8f
Marcos <marcos.fuenmayorhtc@gmail.com>
2021-02-24 16:20:46 -0400

    merging to master

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
+  },
 ];
 
 @NgModule({
